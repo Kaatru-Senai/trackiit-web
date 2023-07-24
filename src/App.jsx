eb187aa91fbdab8f4ec2955b9c2f819d4860657c
Hemnath0075 <hemnath5700@gmail.com>
2023-07-24 23:50:58 +0530

    map fixed moving issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
 import './App.css';
 import LiveMap from './Components/LiveMap';
 import Navbar from './Components/Navbar';
+import Select from 'react-select';
+// import { colourOptions } from '../data';
+
+const options=[
+  {
+    "label":'IIT MAIN GATE',"value":'IIT MAIN GATE',
+  },
+  {
+    "label":'VELACHERY GATE',"value":'VELACHERY GATE',
+  },
+  {
+    "label":'TARAMANI GATE',"value":'TARAMANI GATE',
+  }
+
+]
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <LiveMap/>
+      <div className="flex flex-col flex-auto justify-between">
+        <Select
+          className="basic-single"
+          classNamePrefix="select"
+          defaultValue={options[0]}
+          isClearable={true}
+          isRtl={false}
+          isSearchable={true}
+          name="color"
+          options={options}
+        />
+      <div className="py-4 mx-4 rounded-md">
+        <button className='w-full rounded-md py-4 flex justify-center items-center bg-lime-400 text-white font-bold '>TRACKIIT</button>
+      </div>
+      </div>
     </div>
   );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className="w-[100vw] h-[8vh] border-b-2 border-gray-300">
+    <div className="w-[100vw] h-[6vh] border-b-2 border-gray-300">
         <div className="w-full h-full flex justify-center items-center font-bold text-[5vmin]">
             TRACKIIT
         </div>
